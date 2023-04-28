9387d6de453412de8a0f3bdfd49a03b37f04e23d
MarwaAbuEssa <meromeroo@gmail.com>
2023-04-28 13:56:04 -0700

    Convert hex

--- convert_hex.c
+++ convert_hex.c
@@ -1,79 +1,72 @@
 #include "main.h"
 
 /**
- * convert_x - Converts an unsigned int argument to hex using abcdef
- *             and stores it to a buffer contained in a struct.
- *
- * @args: A va_list pointing to the argument to be converted.
- * @output: A buffer_t struct containing a character array.
- * @flags: Flag modifiers.
- * @width: A width modifier.
- * @precision: A precision modifier.
- * @length: A length modifier.
- *
+ * convert_x - Converts an unsigned int argument to hex
+ * @args: arguments to be converted.
+ * @output: buffer
+ * @flags: Flag.
+ * @width: width
+ * @precision:  precision
+ * @length: length
  * Return: The number of bytes stored to the buffer.
  */
 unsigned int convert_x(va_list args, buffer_t *output, unsigned char flags,
 		int width, int precision, unsigned char length)
 {
-	unsigned long int num;
-	unsigned int ret = 0;
+	unsigned long int number;
+	unsigned int reten = 0;
 	char *lead = "0x";
 
 	if (length == LONG)
-		num = va_arg(args, unsigned long int);
+		number = va_arg(args, unsigned long int);
 	else
-		num = va_arg(args, unsigned int);
+		number = va_arg(args, unsigned int);
 	if (length == SHORT)
-		num = (unsigned short)num;
+		number = (unsigned short)number;
 
-	if (HASH_FLAG == 1 && num != 0)
-		ret += _memcpy(output, lead, 2);
+	if (HASH_FLAG == 1 && number != 0)
+		reten += _memcpy(output, lead, 2);
 
-	if (!(num == 0 && precision == 0))
-		ret += convert_ubase(output, num, "0123456789abcdef",
+	if (!(number == 0 && precision == 0))
+		reten += convert_ubase(output, number, "0123456789abcdef",
 				flags, width, precision);
 
-	ret += print_neg_width(output, ret, flags, width);
-
-	return (ret);
+	reten += print_neg_width(output, reten, flags, width);
+	return (reten);
 }
 
 /**
- * convert_X - Converts an unsigned int argument to hex using ABCDEF
- *             and stores it to a buffer contained in a struct.
- *
- * @args: A va_list pointing to the argument to be converted.
- * @output: A buffer_t struct containing a character array.
- * @flags: Flag modifiers.
- * @width: A width modifier.
- * @precision: A precision modifier.
- * @length: A length modifier.
- *
- * Return: The number of bytes stored to the buffer.
+ * convert_X - Converts an unsigned int argument to hex
+ * @args: arguments
+ * @output: buffer.
+ * @flags: Flag.
+ * @width: width..
+ * @precision: precision.
+ * @length: length
+ * Return: number of bytes.
  */
 unsigned int convert_X(va_list args, buffer_t *output, unsigned char flags,
 		int width, int precision, unsigned char length)
 {
-	unsigned long int num;
-	unsigned int ret = 0;
+	unsigned long int number;
+	unsigned int reten = 0;
 	char *lead = "0X";
 
 	if (length == LONG)
-		num = va_arg(args, unsigned long);
+		number = va_arg(args, unsigned long);
 	else
-		num = va_arg(args, unsigned int);
+		number = va_arg(args, unsigned int);
 	if (length == SHORT)
-		num = (unsigned short)num;
+		number = (unsigned short)number;
 
-	if (HASH_FLAG == 1 && num != 0)
-		ret += _memcpy(output, lead, 2);
+	if (HASH_FLAG == 1 && number != 0)
+		reten += _memcpy(output, lead, 2);
 
-	if (!(num == 0 && precision == 0))
-		ret += convert_ubase(output, num, "0123456789ABCDEF",
+	if (!(number == 0 && precision == 0))
+		reten += convert_ubase(output, number, "0123456789ABCDEF",
 				flags, width, precision);
 
-	ret += print_neg_width(output, ret, flags, width);
+	reten += print_neg_width(output, reten, flags, width);
 
-	return (ret);
+	return (reten);
 }
